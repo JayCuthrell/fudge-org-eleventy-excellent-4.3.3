b4d6b28b1939b68d42887acc642cfc6213b167d7
Jay Cuthrell <jay@cuthrell.com>
2026-08-09 23:30:58 -0400

    audio files

--- eleventy.config.js
+++ eleventy.config.js
@@ -99,7 +99,7 @@ export default async function (eleventyConfig) {
   // --------------------- Passthrough File Copy
 
   // -- same path
-  ['src/assets/fonts/', 'src/assets/images/', 'src/assets/og-images', 'src/admin/'].forEach(path =>
+  ['src/assets/fonts/', 'src/assets/images/', 'src/assets/audio/', 'src/assets/og-images', 'src/admin/'].forEach(path =>
     eleventyConfig.addPassthroughCopy(path)
   );
 
